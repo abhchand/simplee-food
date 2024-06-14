49df96a1fe1a246220eed69a4c9e4672850c94ed
abhchand <me@abhchand.me>
2024-06-13 22:30:43 -0400

    cleanup `Recipe` model, use shared slug logic

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,16 +8,6 @@ class Recipe < ActiveRecord::Base
   validates :slug, presence: true
 
   validate do |recipe|
-    # # ingredients
-    # if recipe.ingredients.present? && !valid_json?(recipe.ingredients)
-    #   errors.add(:ingredients, 'Invalid JSON')
-    # end
-
-    # # steps
-    # if recipe.steps.present? && !valid_json?(recipe.steps)
-    #   errors.add(:steps, 'Invalid JSON')
-    # end
-
     # source_url
     if recipe.source_url.present?
       is_valid = recipe.source_url =~ /\A#{URI.regexp(%w[http https])}\z/
@@ -48,33 +38,6 @@ class Recipe < ActiveRecord::Base
   end
 
   private
-
-  # Attempts to slug-ify the `name` of the recipe
-  #
-  # e.g. 'Idli and Rasam' becomes 'idli-and-rasam'
-  #
-  # If conflicts exist, we add a suffix `-2`, `-3`, etc...
-  def calculate_slug
-    # If we're updating and name didn't change - don't do anything
-    return if persisted? && !name_changed?
-
-    slug = to_slug(name)
-    suffix = 1
-
-    # Keep looping till we find a valid slug name we can use
-    loop do
-      # We never use `-1` as a suffix. We start with `-2`, if needed
-      slug = [slug, suffix == 1 ? nil : suffix].compact.join('-')
-      recipe = Recipe.find_by_slug(slug)
-
-      if recipe.nil?
-        self[:slug] = slug
-        break
-      end
-
-      suffix += 1
-    end
-  end
 
   def valid_json?(json)
     JSON.parse(json)
