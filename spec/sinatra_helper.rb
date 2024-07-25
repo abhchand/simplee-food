e358859dfd50e6b7b396bb8b515c98e0d94ce2e5
abhchand <me@abhchand.me>
2024-07-24 23:19:54 -0400

    lintfix

--- spec/sinatra_helper.rb
+++ spec/sinatra_helper.rb
@@ -1,20 +1,20 @@
-require "fileutils"
-require "pathname"
+require 'fileutils'
+require 'pathname'
 
-ENV["RACK_ENV"] ||= "test"
-ROOT = Pathname.new(File.expand_path("..", __dir__))
+ENV['RACK_ENV'] ||= 'test'
+ROOT = Pathname.new(File.expand_path('..', __dir__))
 
 # Re-create the DB
-TEST_SQLITE_FILE = ROOT.join("sqlite", "simplee_food.test.sqlite3")
+TEST_SQLITE_FILE = ROOT.join('sqlite', 'simplee_food.test.sqlite3')
 FileUtils.rm(TEST_SQLITE_FILE) if File.exist?(TEST_SQLITE_FILE)
 Dir.chdir(ROOT) do
-  %x{bundle exec rake db:create}
-  %x{bundle exec rake db:migrate}
+  `bundle exec rake db:create`
+  `bundle exec rake db:migrate`
 end
 
 # Load the application
-require File.expand_path("../app/app", __dir__)
+require File.expand_path('../app/app', __dir__)
 
 # Load RSpec content
-require_relative "spec_helper"
-Dir[ROOT.join("spec/support/*.rb")].each { |file| require file }
+require_relative 'spec_helper'
+Dir[ROOT.join('spec/support/*.rb')].each { |file| require file }
