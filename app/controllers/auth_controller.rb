fcc486da73279070610c6feb49dc7f13948ede11
abhchand <me@abhchand.me>
2025-04-09 12:52:58 -0400

    preserve original destination when logging in

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -13,7 +13,19 @@ register do
 
       if should_auth && @user.nil?
         flash[:notice] = 'You must sign in first'
-        redirect '/login'
+
+        # The user hit an endpoint that requires authentication, but was not
+        # authenticated. Preserve the original destination in a `dest` param
+        # that can be used to redirect back here after successful login.
+        dest =
+          (
+            if request.fullpath =~ %r{^/}
+              URI.encode_www_form_component(request.fullpath)
+            else
+              '/recipes'
+            end
+          )
+        redirect "/login?dest=#{dest}"
       end
     end
   end
@@ -30,23 +42,42 @@ get '/login' do
 end
 
 post '/login' do
+  # Calculate paths to redirect to on success and failure. If a `dest` param
+  # exists we want to preserve that redirect on login.
+  #
+  # NOTE: The login page embeds `params[:dest]` as a hidden field so it gets
+  # included in the POST data to this endpoint. Even if `dest` is URL encoded,
+  # the rendered page and subsequent POST data automatically decode it. So
+  # params[:dest] will always be URL decoded here (e.g. "/recipes/foo/edit")
+  encoded_dest = URI.encode_www_form_component(params[:dest]) if params[:dest]
+  failure_path = params[:dest] ? "/login?dest=#{encoded_dest}" : '/login'
+  success_path = params[:dest] || '/recipes'
+
+  #
+  # Authenticate
+  #
+
   username = params['login']['username']
   password = params['login']['password']
 
   user = User.find_by_name(username)
   if user.nil?
     flash[:error] = 'Invalid credentials'
-    redirect '/login'
+    redirect to(failure_path)
   end
 
   if user.password != password
     flash[:error] = 'Invalid credentials'
-    redirect '/login'
+    redirect to(failure_path)
   end
+
+  #
+  # Set Session
+  #
 
   session[:user_id] = user.id
   session[:expires_at] = (Time.now + (60 * 60 * 24) * 7).to_i # 7 days from now
-  redirect to('/recipes')
+  redirect to(success_path)
 end
 
 get '/logout' do
